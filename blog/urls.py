f6fb98a922aea0ca02444c388a9073ca6fc1a50d
Luis Videla <luisvid@gmail.com>
2017-04-19 01:56:11 -0300

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from . import views
 
+#http://stackoverflow.com/questions/25907719/noreversematch-exception-redirect-with-keyword-arguments
 
 urlpatterns =[
     url(r'^$', views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
